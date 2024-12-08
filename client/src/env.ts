acba015c4486301fc1665ee92a4bee596ec31524
Hemant Sharma <hemantsharma1498@gmail.com>
2024-12-08 17:59:57 +0530

    changed env.ts

--- client/src/env.ts
+++ client/src/env.ts
@@ -1,8 +1,3 @@
-interface ImportMetaEnv {
- VITE_API_HOST: string;
- VITE_API_PORT: string;
-}
-
 export const config = {
- apiUrl: `http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}`
+    apiUrl: `http://${import.meta.env.VITE_API_HOST}:${import.meta.env.VITE_API_PORT}`
 };
